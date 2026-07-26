6355c7254c5c547bfe736d100d07f8094359782f
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2026-07-26 19:30:35 +0200

    remove stuff

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,3 @@
-use reqwest::blocking::Client;
-use rustls::ClientConfig;
-use webpki_roots::TLS_SERVER_ROOTS;
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,29 +7,6 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     // std::env::set_var("RUST_BACKTRACE", "full");
-
-    rustls::crypto::aws_lc_rs::default_provider()
-        .install_default()
-        .expect("Failed to install rustls crypto provider");
-
-    let mut root_store = rustls::RootCertStore::empty();
-    root_store.extend(TLS_SERVER_ROOTS.iter().cloned());
-
-    let config = ClientConfig::builder()
-        .with_root_certificates(root_store)
-        .with_no_client_auth();
-
-    let client = Client::builder()
-        .use_preconfigured_tls(config)
-        .build()
-        .unwrap();
-
-    client
-        .get("https://rust-lang.org")
-        .send()
-        .unwrap()
-        .text()
-        .unwrap();
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
