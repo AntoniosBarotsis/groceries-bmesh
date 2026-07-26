eedffee1c5d730e44ca9350c0d86df50c78c9211
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2026-07-26 17:40:06 +0200

    fix reqwest?

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+use reqwest::blocking::Client;
+use rustls::ClientConfig;
+use webpki_roots::TLS_SERVER_ROOTS;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,6 +10,31 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // std::env::set_var("RUST_BACKTRACE", "full");
+
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
+
+    let mut root_store = rustls::RootCertStore::empty();
+    root_store.extend(TLS_SERVER_ROOTS.iter().cloned());
+
+    let config = ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+
+    let client = Client::builder()
+        .use_preconfigured_tls(config)
+        .build()
+        .unwrap();
+
+    client
+        .get("https://rust-lang.org")
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
